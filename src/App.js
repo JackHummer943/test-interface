57589952afabb4528b836e9b48c4961d9e3c7a3e
devJ <bessonov9751@gmail.com>
2024-01-18 12:50:50 +0300

    progress-18-01-12-50

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { persons: [], searchText: ' ' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.filterPersons = this.filterPersons.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ searchText: event.target.value });
+  }
+
+  componentDidMount() {
+    fetch('https://634fc8a3df22c2af7b59dc7a.mockapi.io/items')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ persons: data });
+      });
+  }
+
+  filterPersons() {
+    const { searchText, persons } = this.state;
+    const regex = new RegExp(searchText, 'gi');
+
+    return persons.filter((person) => person.name.match(regex));
+  }
+
+  render() {
+    const { persons, searchText } = this.state;
+    const filteredPersons = searchText ? this.filterPersons() : persons;
+
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search by name"
+          value={searchText}
+          onChange={this.handleChange}
+        />
+        <ul>
+          {filteredPersons.map((person, id) => (
+            <li key={id}>
+              {person.id}
+              {person.name}
+              {person.age}
+              {person.city}
+              {person.weight}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
-
 export default App;
